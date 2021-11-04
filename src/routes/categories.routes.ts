b551ec0c587a5cec73bb781cb3a292f00dedd85f
matheusteixeira7 <matheus.teixeira7@gmail.com>
2021-11-04 20:32:09 -0300

    feat: add create category service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { CategoriesRepository } from './../repositories/CategoriesRepository'
 import { Router } from 'express'
+import { CreateCategoryService } from '../service/CreateCategoryService'
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
@@ -7,13 +8,9 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
 
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: 'Category already exists' })
-  }
-
-  categoriesRepository.create({ name, description })
+  createCategoryService.execute({ name, description })
 
   return res.status(201).send()
 })
