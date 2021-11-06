01b8023867d1e442ec3665c6e70e49581bef3b92
matheusteixeira7 <matheus.teixeira7@gmail.com>
2021-11-06 08:32:54 -0300

    refactor: remove postgres repository import from categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,6 @@
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { Router } from 'express'
 import { CreateCategoryService } from '../modules/cars/service/CreateCategoryService'
-import { PostgresCategoriesRepository } from '../modules/cars/repositories/PostgresCategoriesRepository'
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
