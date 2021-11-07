6e763afb89cc45701d162852b5b59a2baf5524a3
matheusteixeira7 <matheus.teixeira7@gmail.com>
2021-11-07 16:59:18 -0300

    chore: add swagger config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 export const app = express()
-import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
 import morgan from 'morgan'
+import { router } from './routes'
 
 // Middlewares
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(morgan('dev'))
 app.use(router)
